526bad1c584477e78f9f9ac4d278f0a5ac8d046a
michele laptop <micheledong2803@gmail.com>
2024-07-01 16:08:52 +0200

    m1 - yield section

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.base')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="/css/style.css">
-</head>
+@section('title', 'About')
 
-<body>
-    @include('partials.header')
-
+@section('content')
     @foreach ($books as $book)
         @if (@$loop->index % 3 == 0)
             <div class="content-wrapper">
@@ -30,7 +21,4 @@
             </div>
         @endif
     @endforeach
-
-</body>
-
-</html>
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>laravel-comics</title>
-    <link rel="stylesheet" href="/css/style.css">
-</head>
-<body>
-    @include('partials.header')
+@extends('layouts.base')
+@section('title', 'Home')
+@section('content')
     <h1>
         Questa è la home ;)
     </h1>
-    
-</body>
-</html>
+@endsection
